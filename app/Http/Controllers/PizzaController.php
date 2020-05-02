4bbf1dac8af6aee94367bde67823056949a06c46
lbarrios1985 <nikeven@gmail.com>
2020-05-01 21:59:18 -0400

    The restoration of the original item's price has been conditioned. Comments added to the order method.

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -102,56 +102,77 @@ class PizzaController extends Controller
      */
     public function order(Request $request, Pizza $pizza)
     {
+        // Keep track of original pizza price
         $original_price = $pizza->price;
+        // Base exchange rate value 1 to 1 (USD to USD)
         $exchange_value = 1;
+        // Parameter present in the request means another currency has been selected
         if ($request->has('currency')) {
             $currency = mb_strtoupper($request['currency'], 'UTF-8');
+            // Try to get the convertion rate between the given currency & USD
             try {
                 $client = new Client();
+                // Free API
                 $exchange = $client->request(
                     'GET',
                     // env('EXCHANGE_RATES_URL')
                     'https://api.exchangeratesapi.io/latest?base=USD&symbols='.$currency
                 );
+                // API response to array
                 $response = json_decode($exchange->getBody()->getContents(), True);
+                // Keep the exchange value
                 $exchange_value = $response['rates'][$currency];
+                // Convert the pizza price to the selected currency
                 $pizza->price = $pizza->price * $exchange_value;
+                // Set the currency in the cart config
                 Config::set('cart_manager.currency', $currency);
             } catch(\Exception $e){
+                // If any error, throw it
                 return response()->json(['error'=>$e->getMessage()], 409);
             }
         }
 
         $qty = 0;
         if ($request->has('qty')) {
+            // The item quantity has been selected
             $qty = $request->qty;
         }
 
         if (isset($currency)) {
+            // When the currency has changed, store the item price temporarily
             $pizza->update();
         }
 
         if (cart()->isEmpty()) {
+            // User hasn't a created cart, create a new one from theselected cartable model item
             $qty == 0 ? $qty = 1 : '';
             $cart = Pizza::addToCart($pizza->id, $qty);
         } else {
+            // A cart already exist to the User, add the selected item
+            // If the item alreay exist nothing will change, except there's a difference on the qty
             cart()->add($pizza, $qty);
         }
+        // Refreshes all items amounts of the Cart
         cart()->refreshAllItemsData();
 
-        $pizza->price = $original_price;
-        $pizza->update();
+        if ($pizza->wasChanged()) {
+            // Restore the original item's price
+            $pizza->price = $original_price;
+            $pizza->update();
+        }
 
+        // Add the currency selected to the custom field on the cart
         DB::table('carts')->where('auth_user', auth('api')->user()->id)->latest()
                           ->update(['currency' => Config::get('cart_manager.currency'),]);
 
-        
         $response = [];
         $response['totals'] = cart()->totals();
         $response['items'] = cart()->items($displayCurrency = true);
 
         if ($request->headers->has('guest-created')) {
+            // A new guest user has been created & the header is set
             $token = $request->header('Authorization');
+            // Extract the Bearer token & pass it to the frontend to keep track of the User's cart
             $response['token'] = str_replace('Bearer ', '', $token);
         }
         return response()->json([$response], 200);
